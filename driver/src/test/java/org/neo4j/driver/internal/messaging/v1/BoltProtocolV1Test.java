910aff1f9a8b27ae8a9315ba9d0c0bb8ae696e2b
lutovich <konstantin.lutovich@neo4j.com>
2018-07-26 18:05:02 +0200

    Expose transaction timeout and metadata in the API
    This is the first draft of the API to allow transaction timeout and/or
    metadata be set for explicit and auto-commit transactions. Both
    settings can be specified in a `TransactionConfig` object which can be
    created like this:
    
    ```
    TransactionConfig config = TransactionConfig.builder()
                .withTimeout(Duration.ofSeconds(5))
                .withMetadata(Collections.singletonMap("key", "value"))
                .build();
    ```
    
    Explicit transactions can take config like this:
    
    ```
    Transaction tx = session.beginTransaction(config);
    ```
    
    and auto-commit transactions accept it in various overloads of
    `Session#run()` and `Session#runAsync()`:
    
    ```
    session.run("CREATE ()", config);
    
    session.runAsync("RETURN $x", Collections.singletonMap("x", 1) config);
    ```
    
    Transaction functions accept config like this:
    
    ```
    session.readTransaction(tx -> {...}, config);
    
    session.writeTransactionAsync(tx -> {...}, config);
    ```
    
    There also exists a possibility to specify default transaction
    configuration per driver. It can be configured in the driver's config:
    
    ```
    Config driverConfig = Config.build()
                .withDefaultTransactionConfig(txConfig)
                .toConfig();
    ```
    
    More tests and javadocs will be added in subsequent commits.

--- driver/src/test/java/org/neo4j/driver/internal/messaging/v1/BoltProtocolV1Test.java
+++ driver/src/test/java/org/neo4j/driver/internal/messaging/v1/BoltProtocolV1Test.java
@@ -52,6 +52,7 @@ import org.neo4j.driver.internal.spi.ResponseHandler;
 import org.neo4j.driver.internal.util.Futures;
 import org.neo4j.driver.v1.Logging;
 import org.neo4j.driver.v1.Statement;
+import org.neo4j.driver.v1.TransactionConfig;
 import org.neo4j.driver.v1.Value;
 
 import static java.util.Collections.emptyMap;
@@ -141,7 +142,7 @@ public class BoltProtocolV1Test
     {
         Connection connection = connectionMock();
 
-        CompletionStage<Void> stage = protocol.beginTransaction( connection, Bookmarks.empty() );
+        CompletionStage<Void> stage = protocol.beginTransaction( connection, Bookmarks.empty(), TransactionConfig.empty() );
 
         verify( connection ).write(
                 new RunMessage( "BEGIN" ), NoOpResponseHandler.INSTANCE,
@@ -156,7 +157,7 @@ public class BoltProtocolV1Test
         Connection connection = connectionMock();
         Bookmarks bookmarks = Bookmarks.from( "neo4j:bookmark:v1:tx100" );
 
-        CompletionStage<Void> stage = protocol.beginTransaction( connection, bookmarks );
+        CompletionStage<Void> stage = protocol.beginTransaction( connection, bookmarks, TransactionConfig.empty() );
 
         verify( connection ).writeAndFlush(
                 eq( new RunMessage( "BEGIN", bookmarks.asBeginTransactionParameters() ) ), eq( NoOpResponseHandler.INSTANCE ),
@@ -247,7 +248,7 @@ public class BoltProtocolV1Test
         if ( autoCommitTx )
         {
 
-            cursorStage = protocol.runInAutoCommitTransaction( connection, STATEMENT, false );
+            cursorStage = protocol.runInAutoCommitTransaction( connection, STATEMENT, Bookmarks.empty(), TransactionConfig.empty(), false );
         }
         else
         {
@@ -267,7 +268,7 @@ public class BoltProtocolV1Test
         CompletionStage<InternalStatementResultCursor> cursorStage;
         if ( session )
         {
-            cursorStage = protocol.runInAutoCommitTransaction( connection, STATEMENT, true );
+            cursorStage = protocol.runInAutoCommitTransaction( connection, STATEMENT, Bookmarks.empty(), TransactionConfig.empty(), true );
         }
         else
         {
